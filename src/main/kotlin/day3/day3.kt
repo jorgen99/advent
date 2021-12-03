65fb4da4e97efeed07b62c6ab6c990b6b0a7dc39
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2021-12-03 16:27:35 +0100

    Day3 step 2

--- src/main/kotlin/day3/day3.kt
+++ src/main/kotlin/day3/day3.kt
@@ -3,18 +3,45 @@ package day3
 import util.readResourceFile
 
 fun main() {
-    val depths = readResourceFile("day3_input.txt")
-//    val depths = readResourceFile("day3_sample.txt")
-    println("depths: ${depths.size}")
-    val result =
-        depths
-            .fold(Accumulator(IntArray(depths[0].length), 0)) { acc, bits -> acc.add(bits) }
+    val values = readResourceFile("day3_input.txt")
+//    val values = readResourceFile("day3_sample.txt")
+    println("values: ${values.size}")
+    val result = gammaAndEpsilon(values)
     println("step1: ${result.power()}")
+    val pair = oxygenAndCo2(values, result.gamma())
+    val oxygenBinary = pair.first
+    val oxy = Integer.parseInt(oxygenBinary, 2)
+    println("oxygen: $oxygenBinary, asInt: $oxy")
+    val co2Binary = pair.second
+    val co2 = Integer.parseInt(co2Binary, 2)
+    println("co2: $co2Binary, asInt: $co2")
+    println("step2: ${oxy * co2}")
+}
+
+fun gammaAndEpsilon(values: List<String>) : Accumulator {
+    return values
+        .fold(Accumulator(IntArray(values[0].length), 0)) { acc, bits -> acc.add(bits) }
+}
+
+fun oxygenAndCo2(values: List<String>, bitCount: String): Pair<String, String> {
+
+    fun filterOnMask(xs: List<String>, index: Int, mask: String, isEpsilon: Boolean): List<String> {
+        if(xs.size == 1 || index > mask.length) {
+            return xs
+        }
+        val remainingValues = xs.filter { it[index] == mask[index] }
+        val accForRest = gammaAndEpsilon(remainingValues)
+        val newMask = if(isEpsilon) accForRest.epsilon() else accForRest.gamma()
+        return filterOnMask(remainingValues, index + 1, newMask, isEpsilon)
+    }
+
+    val oxygen = filterOnMask(values, 0, bitCount, false).first()
+    val co2 = filterOnMask(values, 0, bitCount.invertBinaryString(), true).first()
+    return Pair(oxygen, co2)
 }
 
 class Accumulator(private var bitCount: IntArray, private val count: Int) {
     fun add(bits: String): Accumulator {
-        println(bits)
         if (bitCount.isEmpty()) bitCount = IntArray(bits.length)
         bits
             .map { it.toString().toInt() }
@@ -25,17 +52,23 @@ class Accumulator(private var bitCount: IntArray, private val count: Int) {
     }
 
     fun power(): Int {
-        val half = count / 2
-        val gammaString = bitCount.joinToString("") { if (it > half) "1" else "0" }
-        println("gamma:   $gammaString")
-        val epsilonString = bitCount.joinToString("") { if (it > half) "0" else "1" }
-        println("epsilon: $epsilonString")
+        val gammaString = gamma()
+        val epsilonString = gammaString.invertBinaryString()
         val gamma = gammaString.toBinaryInt()
         val epsilon = epsilonString.toBinaryInt()
-        println("gamma: $gamma, epsilon: $epsilon")
         return gamma * epsilon
     }
+
+    fun gamma(): String {
+        return bitCount.joinToString("") { if (it >= count - it) "1" else "0" }
+    }
+
+    fun epsilon() : String {
+        return gamma().invertBinaryString()
+    }
 }
+
 fun String.toBinaryInt(): Int = Integer.parseInt(this, 2)
-
-
+fun String.invertBinaryString() : String {
+    return this.map { if (it == '1') "0" else "1"}.joinToString("")
+}
