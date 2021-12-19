0f2f4d8f24a1c6454ba6995ce2e5d4946da01602
Jörgen Lundberg <jorgen.lundberg@astrazeneca.com>
2021-12-19 20:53:37 +0100

    Day10, step2

--- src/main/kotlin/day10/day10.kt
+++ src/main/kotlin/day10/day10.kt
@@ -1,39 +1,96 @@
 package day10
 
-fun syntaxErrorScore(sample: List<String>) =
-    sample.map { isCorrupt(it) }
-        .filter { (i, _) -> i != -1 }
-        .sumOf { (_, p) -> p }
+fun middleCompletionScore(chunks: List<String>): Long {
+    val sorted = chunks
+        .asSequence()
+        .map { it to findCorruption(it) }
+        .filter { (_, p) -> p.first == -1 }
+        .map { (chunk, _) -> finishIncomplete(chunk) }
+        .map { score(it) }
+        .sorted()
+        .toList()
+    return sorted
+        .drop(sorted.size / 2)
+        .first()
+}
 
-fun isCorrupt(line: String): Pair<Int, Int> {
+fun score(completedBy: String): Long {
+    return completedBy
+        .map { completeScore[it] }
+        .fold(0L) { acc, i -> acc * 5 + (i ?: 0) }
+}
+
+fun finishIncomplete(line: String): String {
     val open = ArrayDeque<Char>()
     for (i in line.indices) {
         val c = line[i]
         if (open.isEmpty()) {
-            if (c in closing.keys) return i to (points[c] ?: 0)
+            if (c in matchingOpening.keys)
+                throw RuntimeException("This should have been detected")
             open.addFirst(c)
             continue
         }
-        if (c in closing.values) {
+        if (c in matchingOpening.values) {
             open.addFirst(c)
             continue
         }
         val first = open.first()
-        val matching = first == closing[c]
-        if (matching) open.removeFirst() else return i to (points[c] ?: 0)
+        val matching = first == matchingOpening[c]
+        if (!matching)
+            throw RuntimeException("This should have been detected")
+
+        open.removeFirst()
+    }
+    return open.map { matchingClosing[it] }
+        .joinToString("")
+}
+
+fun syntaxErrorScore(chunks: List<String>) =
+    chunks.map { findCorruption(it) }
+        .filter { (i, _) -> i != -1 }
+        .sumOf { (_, p) -> p }
+
+fun findCorruption(line: String): Pair<Int, Int> {
+    val open = ArrayDeque<Char>()
+    for (i in line.indices) {
+        val c = line[i]
+        if (open.isEmpty()) {
+            if (c in matchingOpening.keys) return i to (invalidScore[c] ?: 0)
+            open.addFirst(c)
+            continue
+        }
+        if (c in matchingOpening.values) {
+            open.addFirst(c)
+            continue
+        }
+        val first = open.first()
+        val matching = first == matchingOpening[c]
+        if (matching) open.removeFirst() else return i to (invalidScore[c] ?: 0)
     }
     return -1 to 1
 }
 
-private val closing = mapOf(
+private val matchingOpening = mapOf(
     ')' to '(',
     ']' to '[',
     '}' to '{',
     '>' to '<'
 )
-private val points = mapOf(
+private val matchingClosing = mapOf(
+    '(' to ')',
+    '[' to ']',
+    '{' to '}',
+    '<' to '>'
+)
+private val invalidScore = mapOf(
     ')' to 3,
     ']' to 57,
     '}' to 1197,
     '>' to 25137
+)
+private val completeScore = mapOf(
+    ')' to 1,
+    ']' to 2,
+    '}' to 3,
+    '>' to 4
 )
--- src/test/kotlin/day10/Day10Test.kt
+++ src/test/kotlin/day10/Day10Test.kt
@@ -6,7 +6,7 @@ import util.readResourceFile
 
 internal class Day10Test {
 
-    val sample = """
+    private val sample = """
         [({(<(())[]>[[{[]{<()<>>
         [(()[<>])]({[<{<<[]>>(
         {([(<{}[<>[]}>{[]{[(<()>
@@ -21,11 +21,57 @@ internal class Day10Test {
         .trimIndent()
         .lines()
 
+    data class Sample(val chunks: String, val complete: String, val score: Int)
+
+    private val sample2 = """
+        [({(<(())[]>[[{[]{<()<>>  ;   }}]])})]    ;   288957 
+        [(()[<>])]({[<{<<[]>>(    ;   )}>]})      ;   5566
+        (((({<>}<{<{<>}{[]{[]{}   ;   }}>}>))))   ;   1480781
+        {<[[]]>}<{[{[{[]{()[[[]   ;   ]]}}]}]}>   ;   995444
+        <{([{{}}[<[[[<>{}]]]>[]]  ;   ])}>        ;   294
+    """.trimIndent()
+        .lines()
+        .asSequence()
+        .map { it.split(Regex(";")) }
+        .flatten()
+        .map { it.trim() }
+        .chunked(3)
+        .map { Sample(it[0], it[1], it[2].toInt()) }
+        .toList()
+
+
+    @Test
+    fun it_should_calculate_the_ax_error_score_for_the_input() {
+        val lines = readResourceFile("day10_input.txt")
+        val sum = middleCompletionScore(lines)
+        assertThat(sum).isEqualTo(2391385187)
+    }
+
+    @Test
+    fun it_should_score() {
+        val score = score("])]})>)]]}}}>]")
+        assertThat(score).isEqualTo(2816133597L)
+    }
+
+    @Test
+    fun it_should_find_sing_braces_for_the_sample() {
+        val middleScore = middleCompletionScore(sample)
+        assertThat(middleScore).isEqualTo(288957)
+    }
+
+    @Test
+    fun it_should_find_closing_braces_for_the_sample() {
+        sample2.forEach { (chunks, complete, _) ->
+            assertThat(finishIncomplete(chunks)).isEqualTo(complete)
+        }
+    }
+
+
     @Test
     fun it_should_find_no_corrupted_lines_in_the_legal_chunks() {
         val anyCorrupt =
             listOf("([])", "{()()()}", "<([{}])>", "[<>({}){}[([])<>]]", "(((((((((())))))))))")
-                .map { isCorrupt(it) }
+                .map { findCorruption(it) }
                 .filter { (i, _) -> i != -1 }
                 .map { (_, c) -> c }
 
